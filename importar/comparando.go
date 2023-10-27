bb05d3477a8550f64150a9c027bd981472484c96
AnaJuliaNX <anajulia.negrixavier@gmail.com>
2023-10-27 17:06:01 -0300

    Todas as rotas principais de POST, PUT, GET, DELETE feitas

--- importar/comparando.go
+++ importar/comparando.go
@@ -7,8 +7,8 @@ import (
 )
 
 // Tentativa de importar, ler e executar sobre o arquivo xml
-func importar() {
-	aquivoXML, erro := os.Open("arquivoXML.xml")
+func main() {
+	aquivoXML, erro := os.Open("/home/ana/Downloads/41230910541434000152550010000012411749316397-nfe.xml")
 	if erro != nil {
 		log.Fatalf("Erro ao abrir o arquivo xml")
 	} else {
@@ -16,3 +16,6 @@ func importar() {
 	}
 	defer aquivoXML.Close()
 }
+
+// type destinatario struct {
+// }
